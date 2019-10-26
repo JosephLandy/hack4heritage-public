0be1579545e9ef611bdc982dcbf235cd71edf6a0
natpauze <45314250+natpauze@users.noreply.github.com>
2019-10-26 11:03:07 -0400

    made particle textures mat and shader, set up new headset, player scale can be adjusted

---
+++ Assets/scripts/PlayerScaleController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+ * Class for changing player scale at run time 
+ */
+public class PlayerScaleController : MonoBehaviour
+{
+    [SerializeField]
+    GameObject playerGameObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/scripts/SystemManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SystemManager : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject playerGameObject;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (playerGameObject == null)
+        {
+            Debug.LogError("please assign player gameobject");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.G))
+        {
+            playerGameObject.transform.localScale += new Vector3(1, 1, 1) * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.B))
+        {
+            playerGameObject.transform.localScale -= new Vector3(1, 1, 1) * Time.deltaTime;
+        }
+    }
+}

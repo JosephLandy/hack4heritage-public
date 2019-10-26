817ed4994124b56f6acdd1573407e585342d164c
natpauze <45314250+natpauze@users.noreply.github.com>
2019-10-26 14:27:00 -0400

    more steam vr shenanigans

--- Assets/scripts/PlaceAbleObject.cs
+++ Assets/scripts/PlaceAbleObject.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Valve.VR;
 
 
 /*
@@ -12,6 +12,35 @@ using UnityEngine;
  */
 public class PlaceAbleObject : MonoBehaviour
 {
+
+    /*public SteamVR_Action_Boolean grabPinch; //Grab Pinch is the trigger, select from inspecter
+    public SteamVR_Input_Sources inputSource = SteamVR_Input_Sources.Any;//which controller
+                                                                         // Use this for initialization
+
+    void OnEnable()
+    {
+        if (grabPinch != null)
+        {
+            grabPinch.AddOnStateDownListener(menuButtonDown, inputSource);
+        }
+    }
+
+
+    private void OnDisable()
+    {
+        if (grabPinch != null)
+        {
+            grabPinch.RemoveOnStateDownListener(menuButtonDown, inputSource);
+        }
+    }*/
+
+
+
+    public SteamVR_Input_Sources handType; // 1
+    public SteamVR_Action_Boolean MenuButton; // 2
+
+
+
     [SerializeField]
     public AnimationData animationData;
     [SerializeField]
@@ -26,16 +55,32 @@ public class PlaceAbleObject : MonoBehaviour
     AnimationPoint currentAnimationPoint;
     [SerializeField]
     float animationTransitionPercent;
+    [SerializeField]
+    public bool isBeingHeld = false;
     // Start is called before the first frame update
     void Start()
     {
         
     }
+    //another hack so we knwo when we are being picked up
+    public void pickedup()
+    {
+        isBeingHeld = true;
+    }
+    public void droped()
+    {
+        isBeingHeld = false;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        currentTime = Time.deltaTime;
+        if (GetMenuDown())
+        {
+            Debug.Log("menu button pressed");
+        }
+
+        currentTime += Time.deltaTime;
 
         if (isRecordingMode)
         {
@@ -46,13 +91,29 @@ public class PlaceAbleObject : MonoBehaviour
         }
         else
         {
+            //super messy way to reintilize animation index
+            if(currentTime < 0.1)
+            {
+                currentAnimationIndex = 0;
+                currentAnimationPoint = animationData.animationPoints[0];
+            }
             //if there are still aniamtion points 
-            if(currentAnimationIndex < animationData.animationPoints.Count)
+            if(currentAnimationIndex < animationData.animationPoints.Count-1)
             {
                 //this is the next point
                 AnimationPoint nextAnimationPoint = animationData.animationPoints[currentAnimationIndex +1];
+                //calculate the transition percent btween the current point and the point we are moving to 
                 animationTransitionPercent =   (currentTime - currentAnimationPoint.time)/(nextAnimationPoint.time - currentAnimationPoint.time) ;
-                //this.transform.position = 
+                //lerp the transforms 
+                this.transform.position = Vector3.Lerp(currentAnimationPoint.position, nextAnimationPoint.position, animationTransitionPercent);
+                this.transform.eulerAngles = Vector3.Lerp(currentAnimationPoint.rotation, nextAnimationPoint.rotation, animationTransitionPercent);
+                this.transform.localScale = Vector3.Lerp(currentAnimationPoint.scale, nextAnimationPoint.scale, animationTransitionPercent);
+                //go to the next one if this animation is done
+                if (animationTransitionPercent > 0.99)
+                {
+                    currentAnimationPoint = nextAnimationPoint;//move the current animtion point forward
+                    currentAnimationIndex++;
+                }
 
                 
             }
@@ -62,6 +123,15 @@ public class PlaceAbleObject : MonoBehaviour
 
 
     }
+    public bool GetMenuDown() // 1
+    {
+        return MenuButton.GetStateDown(handType);
+    }
+
+    /*private void menuButtonDown(SteamVR_Action_In action_In)
+    {
+        recordTransform();
+    }*/
 
     //saves the current transform to animation data 
     void recordTransform()

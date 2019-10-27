9b617884bcdf9f15651990cb310fb58babf52af9
JosephLandy <joseph.landy.email@gmail.com>
2019-10-26 20:14:45 -0400

    added mask asset

---
+++ Assets/SpawningInterface.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawningInterface : MonoBehaviour
+{
+
+    public List<GameObject> props;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
